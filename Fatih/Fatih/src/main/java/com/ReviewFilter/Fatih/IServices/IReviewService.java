c6b1a3697338af50e09af453394219bc15ccfc9e
Fatih Emurlai <102032599+Alpha-0111@users.noreply.github.com>
2022-06-27 23:29:39 +0200

    Add files via upload

---
+++ Fatih/Fatih/src/main/java/com/ReviewFilter/Fatih/IServices/IReviewService.java
@@ -0,0 +1,11 @@
+package com.ReviewFilter.Fatih.IServices;
+
+import java.util.List;
+
+import com.ReviewFilter.Fatih.Models.Review;
+
+public interface IReviewService {
+
+    public List<Review> GettAllReviews();
+    
+}
---
+++ Fatih/Fatih/src/test/java/com/ReviewFilter/Fatih/FatihApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReviewFilter.Fatih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
